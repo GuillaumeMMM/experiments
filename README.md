a6a333692fb1dc7feadf5ae0d92a1dfeb9a61b8a
GuillaumeMMM <guillaume.meigniez@gmail.com>
2026-02-11 11:40:12 +0900

    Restructure and add Readme.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dev experiments
+
+For now this repo only contains experiments using [d3.js](https://d3js.org/) along with the [data-navigator](https://dig.cmu.edu/data-navigator/) library in order to make data visualizations more accessible.
+
+These are only experiments. As such they do not always meet accessibility requirements from [WCAG](https://www.w3.org/WAI/standards-guidelines/wcag/), [EN 301 549](https://accessible-eu-centre.ec.europa.eu/content-corner/digital-library/en-3015492021-accessibility-requirements-ict-products-and-services_en) or [Chartability](https://chartability.github.io/POUR-CAF/).
+
+## Run the examples locally
+
+```
+npm install
+npm start
+```
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                ['bar-chart']: resolve(__dirname, 'examples/bar-chart/index.html'),
-                ['stacked-bar-chart']: resolve(__dirname, 'examples/stacked-bar-chart/index.html'),
-                ['scatter-plot']: resolve(__dirname, 'examples/scatter-plot/index.html'),
-                ['map']: resolve(__dirname, 'examples/map/index.html'),
+                ['bar-chart']: resolve(__dirname, 'examples/data-navigator/bar-chart/index.html'),
+                ['stacked-bar-chart']: resolve(__dirname, 'examples/data-navigator/stacked-bar-chart/index.html'),
+                ['scatter-plot']: resolve(__dirname, 'examples/data-navigator/scatter-plot/index.html'),
+                ['map']: resolve(__dirname, 'examples/data-navigator/map/index.html'),
             }
         }
     }
